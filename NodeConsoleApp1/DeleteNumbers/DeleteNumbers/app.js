9d80336bd364cda28ff225c7ab4d784f6ef7dbe8
Sara <sara.isabel.jordana@gmail.com>
2026-05-13 22:08:05 +0200

    lilisto ew

---
+++ NodeConsoleApp1/DeleteNumbers/DeleteNumbers/app.js
@@ -0,0 +1,19 @@
+function eliminarDuplicados(arr) {
+
+  const resultado = [];
+
+  for (let i = 0; i < arr.length; i++) {
+
+    if (!resultado.includes(arr[i])) {
+
+      resultado.push(arr[i]);
+
+    }
+  }
+
+  return resultado;
+}
+
+console.log(
+  eliminarDuplicados([1,2,2,3,4,4])
+);
